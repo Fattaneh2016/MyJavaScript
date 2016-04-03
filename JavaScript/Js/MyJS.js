3e67e1563577f372590fd6c51d4834d4ca7ecbd0
Fattaneh2016 <fatameri@hotmail.com>
2016-04-03 00:32:45 -0400

    Event in javascript

--- JavaScript/Js/MyJS.js
+++ JavaScript/Js/MyJS.js
@@ -13,18 +13,24 @@ function Mytest() {
     $elem.innerHTML += 4 + 5 ;
 }
 
+function myArrayTwo() {
+    var Cars = ["volovo", "benz", "Nissan"];
 
-var Cars = ["volovo", "benz", "Nissan"];
+    var fruits = new array("apple", "banana", "orange");
+}
 
-var fruits = new array("apple", "banana", "orange");
 
 function myFunc() {
 
 };
 
-myArray[0] = Date.now;
-myArray[1] = myFunc;
-myArray[2] = new array("rose", "narges");
+function myArrayOne() {
+
+    myArray[0] = Date.now;
+    myArray[1] = myFunc;
+    myArray[2] = new array("rose", "narges");
+
+}
 
 
 function myTestVariable() {
@@ -42,10 +48,64 @@ function myTestVariable() {
 
 }
 
-function saveTextValue() {
+function saveToCookie(key, value, onCompletion) {
+    var cookieValue = key + ';' + value;
+    document.cookie += cookieValue;
+    onCompletion(cookieValue);
+}
 
+function saveTextValue() {
+    var textValue = document.getElementsByName('position')[0].value;
+    saveToCookie('Position', textValue, showTextSuccess);
+}
+
+function showTextSuccess(result) {
+    window.alert('You successfully saved [' + result + '] from the text input to your cookie.');
 }
 
 function saveRadioValue() {
-
+    var radioValue;
+    var radioOptions = document.getElementsByName('department');
+    for (var index = 0; index < radioOptions.length; index++) {
+        if (radioOptions[index].checked) {
+            radioValue = radioOptions[index].value;
+            break;
+        }
+    }
+    
+    saveToCookie('Department', radioValue, function (result) {
+        window.alert('You did it! You saved [' + result + ']');
+    });   
 }
+
+function handleClick(callback) {
+    alert('This button has been click');
+    if (callback) {
+        callback();
+    }
+}
+
+function doMore() {
+    alert('I need to do more logic here')
+}
+function doSomethingElse() {
+    document.writeln('Do something here');
+}
+
+document.getElementById('dateButton').onclick = handleClick;
+function handleClick() {
+    document.getElementById('dateTarget').innerHTML = Date();
+    
+    
+//}
+
+//Named Identifier:
+//document.getElementById('dateButton').addEventListener('click', handleClick);
+//function handleClick() {
+//    document.getElementById('dateTarget').innerHTML = Date();
+}
+
+//Anonymous Function:
+//document.getElementById('dateButton').addEventListener('click', function handleClick() {
+//    document.getElementById('dateTarget').innerHTML = Date();
+//});
