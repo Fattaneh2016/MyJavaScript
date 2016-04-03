4d2ebb4128cfdf682961731d2aa58954baa09226
Fattaneh2016 <fatameri@hotmail.com>
2016-04-02 20:37:27 -0400

    javascript variables

---
+++ JavaScript/Js/MyJS.js
@@ -0,0 +1,51 @@
+﻿function startLogic() {
+    var newHtml = '<header>New Header</header>';
+    document.getElementById('headerItem').innerHTML = newHtml;
+}
+
+function clickHandler() {
+    alert('You clicked me!');
+}
+
+function Mytest() {
+    var $elem = document.getElementById('output');
+    $elem.innerHTML = 'hello fati  ';
+    $elem.innerHTML += 4 + 5 ;
+}
+
+
+var Cars = ["volovo", "benz", "Nissan"];
+
+var fruits = new array("apple", "banana", "orange");
+
+function myFunc() {
+
+};
+
+myArray[0] = Date.now;
+myArray[1] = myFunc;
+myArray[2] = new array("rose", "narges");
+
+
+function myTestVariable() {
+
+    var testVariable;
+
+    var isUndefined = (testVariable === undefined);
+    var isNull = (testVariable === null);
+    var isEmpty = (testVariable === '');
+
+
+    document.getElementById('isUndefined').innerHTML = isUndefined;
+    document.getElementById('isNull').innerHTML = isNull;
+    document.getElementById('isEmpty').innerHTML = isEmpty;
+
+}
+
+function saveTextValue() {
+
+}
+
+function saveRadioValue() {
+
+}
